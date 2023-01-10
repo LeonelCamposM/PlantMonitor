815681f290fcab3d69d71e60d6108b937c0a4f7d
LeonelCamposM <leonelscamposm@gmail.com>
2023-01-10 17:00:32 -0600

    handle app routing

--- mobile/lib/presentation/home.dart
+++ mobile/lib/presentation/home.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/infraestructure/soil_meassure_widget.dart';
 import 'package:mobile/presentation/sensor_configuration.dart/configurationForm.dart';
 
+enum NavigationState { home, configurationForm, wifiDashboard, apDashboard }
+
 class Home extends StatelessWidget {
-  const Home({super.key});
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -26,22 +29,66 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  NavigationState navState = NavigationState.configurationForm;
+  var args;
+
   @override
   void initState() {
     super.initState();
   }
 
+  callback(NavigationState state, dynamic args) {
+    print("callback");
+    print(state);
+    setState(() {
+      this.navState = state;
+      this.args = args;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget page = const ConfigurationForm();
+
+    switch (navState) {
+      case NavigationState.home:
+        page = const Center(child: Text("home"));
+        break;
+      case NavigationState.configurationForm:
+        page = const ConfigurationForm();
+        break;
+      case NavigationState.wifiDashboard:
+        page = Center(child: SoilMeassureWidget());
+        break;
+      case NavigationState.apDashboard:
+        page = const Center(child: Text("ap"));
+        break;
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: const [ConfigurationForm()],
-        ),
+      body: Column(
+        children: [
+          page,
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: navState.index,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Inicio"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: "Configuración"),
+          BottomNavigationBarItem(icon: Icon(Icons.wifi), label: "wifi"),
+          BottomNavigationBarItem(icon: Icon(Icons.wifi_off), label: "ap"),
+        ],
+        onTap: (pagina) {
+          setState(() {
+            navState = NavigationState.values[pagina];
+          });
+        },
       ),
     );
   }
