9bd3d80c64e46951077513ff7f1ecc6f4502970b
LeonelCamposM <leonelscamposm@gmail.com>
2023-01-22 18:35:42 -0600

    allow notifications for andorid and pc

--- mobile/web/firebase-messaging-sw.js
+++ mobile/web/firebase-messaging-sw.js
@@ -18,12 +18,4 @@ const messaging = firebase.messaging();
 // Optional:
 messaging.onBackgroundMessage((message) => {
   console.log("onBackgroundMessage", message);
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
-
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
 });
