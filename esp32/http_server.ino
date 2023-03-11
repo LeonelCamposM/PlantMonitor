07eefab88fa0f3df42e3812ef8601fdc32abbb4d
LeonelCamposM <leonelscamposm@gmail.com>
2023-03-10 19:12:40 -0600

    update requests

--- esp32/http_server.ino
+++ esp32/http_server.ino
@@ -11,24 +11,13 @@ const char* PARAM_MESSAGE = "limits";
 AsyncWebServer server(80);
 
 void startHttpServer() {
-
-  server.on("/onUpdatePreferences", HTTP_GET, [](AsyncWebServerRequest* request) {
-    String message;
-    if (request->hasParam(PARAM_MESSAGE)) {
-      message = request->getParam(PARAM_MESSAGE)->value();
-      saveData(PREFERENCES_PATH, message, "");
-    } else {
-      message = "No message sent";
-    }
-    request->send(200, "text/plain", "Receive: " + message);
-  });
-
-  server.on("/onDeleteAllData", HTTP_GET, [](AsyncWebServerRequest* request) {
+  
+  server.on("/deleteAllData", HTTP_GET, [](AsyncWebServerRequest* request) {
     bool response = remove_file(MEASURE_PATH);
     if(response){
-      request->send(200, "text/plain", "200");
+      request->send(200, "text/plain", "ok");
     } else {
-      request->send(200, "text/plain", "404");
+      request->send(404, "text/plain", "error");
     }
   });
 
